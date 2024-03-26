1bc78c651efcd3fcbed8e3dbef2111330f65b304
ScheurmannTiago <scheurmanntiago@outlook.com>
2024-03-26 10:25:21 -0300

    Curso DIO

---
+++ Controle-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"LUCAS", "JULIANA", "BÁRBARA", "TIAGO"};
+
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
